95b02b02155d5f0efe33443bdd48c0c3cdf5392a
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 13:14:30 -0400

    Change about

--- components/about.js
+++ components/about.js
@@ -3,20 +3,21 @@ import profilePic from '../public/me.jpg'
 
 export default function About() {
     return (
-        <div className="flex-col justify-items-center py-10 px-4">
+        <div className="section-container">
+            <h1>Hello, I&apos;m Felipe!</h1>
 
-            <h1 className="flex justify-center py-5 text-6xl font-roboto font-black text-white">
-                Hello, I&apos;m Felipe!
-            </h1>
-
-            <div className="flex justify-center py-5">
+            <div className="flex justify-center py-10">
                 <Image className="w-60 h-60 rounded-full shadow-xl shadow-black" src={profilePic} alt="Picture of the Felipe" />
             </div>
 
-            <p className="text-2xl py-5 px-4 font-roboto text-white">
-                I&apos;m a full-stack developer focused on building functional and valuable web applications.
-                I&apos;m greatly inspired by the idea of developing web applications that can solve real-life problems and help businesses achieve their goals.
-                My strengths in programming gravitate around JavaScript full stack technologies, such as MongoDB, Express.js, React, and Node.js (the MERN stack).
+            <p className="md:w-2/3 md:mx-auto">
+                I&apos;m a full-stack developer focused on building useful and attractive web applications. 
+                I&apos;m particularly inspired by the idea of developing aesthetically appealing web applications 
+                that can solve real-life problems to help individuals and organizations achieve their goals.
+                <br /><br />
+                My strengths in the programming field gravitate around JavaScript full-stack technologies, 
+                such as MongoDB, Express.js, React, and Node.js (the MERN stack). Additionally, my day-to-day toolkit 
+                includes the following technologies: Next.js, TailwindCSS, Git, GitHub, and Visual Studio Code.
             </p>
 
         </div>
