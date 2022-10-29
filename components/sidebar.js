063ea52130a62e8e7b4c5047ea4a2be05358ea40
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 00:17:17 -0400

    Change sidebar to add skills

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,9 +1,10 @@
-import { IoMenu, IoHome, IoSchool, IoDocumentText, IoTerminal, IoFileTrayStacked, IoChatbubbles, IoSunnyOutline, IoSunny } from "react-icons/io5";
+import { IoMenu, IoHome, IoPerson, IoSchool, IoBriefcase, IoDocumentText, IoTerminal, IoFileTrayStacked, IoChatbubbles, IoSunnyOutline, IoSunny } from "react-icons/io5";
 
 export default function Sidebar() {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
-            <SidebarElement icon={<IoHome size="28" />} text="Home"/>
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg border-r-secondary border-r-2">
+            <SidebarElement icon={<IoPerson size="28" />} text="About"/>
+            <SidebarElement icon={<IoBriefcase size="28" />} text="Skills"/>
             <SidebarElement icon={<IoSchool size="28" />} text="Resume"/>
             <SidebarElement icon={<IoTerminal size="28" />} text="Projects"/>
             <SidebarElement icon={<IoChatbubbles size="28" />} text="Contact"/>
