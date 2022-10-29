25c07473a63b79501cc1244fc03c6eb2fc917d95
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 13:30:29 -0400

    Add contact base component

--- components/contact.js
+++ components/contact.js
@@ -1,5 +1,13 @@
+import Card from "./card"
+
 export default function Contact() {
     return (
-        <p>Contact</p>
+        <div className="section-container mb-10">
+            <h1>Contact me</h1>
+
+            <div className="flex-container">
+                <Card title="Coming soon" text="(Work in progress)"/>
+            </div>
+        </div>
     )
 }
