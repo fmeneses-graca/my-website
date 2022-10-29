cad47c86af0480288e7fb6393844075f028f348a
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 16:02:26 -0400

    Add form base to contacts (need style)

--- components/contact.js
+++ components/contact.js
@@ -1,13 +1,53 @@
-import Card from "./card"
+import { useState } from 'react'
 
 export default function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            message,
+        }
+        console.log(data);
+    }
+
     return (
         <div id="contact" className="section-container mb-10">
             <h1>Contact me</h1>
 
+            
+
             <div className="flex-container">
-                <Card title="Coming soon" text="(Work in progress)"/>
+                <p>work in progress</p>
+                
+                {/* <form onSubmit={handleSubmit} className="card">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        id="email"
+                        type="email"
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                        id="message"
+                        type="text"
+                        rows="4"
+                        onChange={e => setMessage(e.target.value)}
+                    />
+                    <button type="submit">Send</button>
+                </form> */}
             </div>
+
         </div>
     )
 }
