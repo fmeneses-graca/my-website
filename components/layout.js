734b1eb65a86a9cfe91b5baf494e30de10569265
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 00:18:25 -0400

    Change layout to add components

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,29 @@
+import About from "./about"
+import Contact from "./contact"
+import Projects from "./projects"
+import Resume from "./resume"
 import Sidebar from "./sidebar"
+import Skills from "./skills"
 
-export default function Layout({ children }) {
+export default function Layout() {
     return (
         <>
             <Sidebar />
-            <main>{children}</main>
+            <div className="text-secondary flex-col pl-20 bg-primary">
+                <About />
+                <HorizontalLine />
+                <Skills />
+                <HorizontalLine />
+                <Resume />
+                <Projects />
+                <Contact />
+            </div>
         </>
+    )
+}
+
+function HorizontalLine() {
+    return (
+        <hr className="mx-auto w-1/2 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700"></hr>
     )
 }
