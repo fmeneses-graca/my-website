d9fee39a079a3e02819c497aba820e9672a13b4b
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 13:15:50 -0400

    Refactor skills.js

--- components/skills.js
+++ components/skills.js
@@ -1,33 +1,33 @@
 export default function Skills() {
     return (
-        <div className="flex-col justify-items-center py-10 px-4">
+        <div className="section-container">
+            <h1>My Developer Skills</h1>
 
-            <h2 className="flex justify-center py-5 px-4 text-4xl font-roboto font-black text-white">My Developer Skills</h2>
-
-            <div className="flex flex-wrap py-10 px-4 justify-between">
+            <div className="flex-container py-10 md:w-2/3 md:mx-auto">
                 <SkillElement text="JavaScript" />
                 <SkillElement text="React" />
                 <SkillElement text="Next.js" />
+                <SkillElement text="HTML" />
+                <SkillElement text="CSS" />
                 <SkillElement text="TailwindCSS" />
                 <SkillElement text="Bootstrap" />
-                <SkillElement text="CSS" />
                 <SkillElement text="REST APIs" />
                 <SkillElement text="Express.js" />
                 <SkillElement text="Node.js" />
+                <SkillElement text="SQL" />
                 <SkillElement text="MongoDB" />
                 <SkillElement text="PostgreSQL" />
                 <SkillElement text="Git" />
                 <SkillElement text="GitHub" />
                 <SkillElement text="Visual Studio Code" />
             </div>
-
         </div>
     )
 }
 
 function SkillElement({ text }) {
     return (
-        <div className="px-5 py-2 m-4 bg-secondary rounded text-2xl font-roboto text-white shadow-lg shadow-black">
+        <div className="skill-element">
             {text}
         </div>
     )
