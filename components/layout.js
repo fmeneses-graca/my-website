b4ae4b3716cd6df644ecf6ce74f21ecaa27184a7
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-10-29 13:15:23 -0400

    Refactor layout

--- components/layout.js
+++ components/layout.js
@@ -1,29 +1,27 @@
 import About from "./about"
 import Contact from "./contact"
 import Projects from "./projects"
-import Resume from "./resume"
-import Sidebar from "./sidebar"
+import Experience from "./experience"
+import Navbar from "./navbar"
 import Skills from "./skills"
+import HorizontalLine from "./horizontal-line"
 
 export default function Layout() {
     return (
         <>
-            <Sidebar />
-            <div className="text-secondary flex-col pl-20 bg-primary">
+            <Navbar />
+            <div className="bg-primary md:pl-20">
                 <About />
                 <HorizontalLine />
                 <Skills />
                 <HorizontalLine />
-                <Resume />
+                <Experience />
+                <HorizontalLine />
                 <Projects />
+                <HorizontalLine />
                 <Contact />
+                <HorizontalLine />
             </div>
         </>
-    )
-}
-
-function HorizontalLine() {
-    return (
-        <hr className="mx-auto w-1/2 h-1 bg-gray-100 rounded border-0 md:my-10 dark:bg-gray-700"></hr>
     )
 }
