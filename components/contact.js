d7338429e00bd0a33248dc86e61d4a34a3afa327
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-11-06 15:37:33 -0500

    Change form width

--- components/contact.js
+++ components/contact.js
@@ -96,7 +96,7 @@ export default function Contact() {
     };
 
     return (
-        <div id="contact" className="section-container mb-10">
+        <div id="contact" className="section-container mb-10 md:w-4/5 md:mx-auto ">
             <form onSubmit={handleSubmit} className="flex flex-col px-8 py-8 my-10 rounded-lg shadow-xl bg-secondary bg-opacity-80 md:mx-40">
 
                 <h1 className="text-2xl font-bold">Send me a message</h1>
