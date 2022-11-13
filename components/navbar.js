36dffb9be7a081a0242f2ace788807bdafcec70e
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2022-11-13 13:00:56 -0500

    Refactor navbar code

--- components/navbar.js
+++ components/navbar.js
@@ -4,31 +4,11 @@ import { Link, animateScroll as scroll } from "react-scroll"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <NavbarElement
-                target="about"
-                icon={<IoPerson size="28" />}
-                text="About"
-            />
-            <NavbarElement
-                target="skills"
-                icon={<IoBriefcase size="28" />}
-                text="Skills"
-            />
-            <NavbarElement
-                target="projects"
-                icon={<IoTerminal size="28" />}
-                text="Projects"
-            />
-            <NavbarElement
-                target="experience"
-                icon={<IoSchool size="28" />}
-                text="Experience"
-            />
-            <NavbarElement
-                target="contact"
-                icon={<IoChatbubbles size="28" />}
-                text="Contact"
-            />
+            <NavbarElement target="about" icon={<IoPerson size="28" />} text="About" />
+            <NavbarElement target="skills" icon={<IoBriefcase size="28" />} text="Skills" />
+            <NavbarElement target="projects" icon={<IoTerminal size="28" />} text="Projects" />
+            <NavbarElement target="experience" icon={<IoSchool size="28" />} text="Experience" />
+            <NavbarElement target="contact" icon={<IoChatbubbles size="28" />} text="Contact" />
             {/* <NavbarElement icon={<IoSunnyOutline size="28" />} text="Dark mode"/> */}
         </nav>
     )
