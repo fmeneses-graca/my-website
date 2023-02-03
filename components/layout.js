300a3766968f85ec0f51c93df1c397c6eca3a793
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2023-02-02 20:18:05 -0500

    Update layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,7 @@
 import AboutSection from "./AboutSection"
 import ContactSection from "./ContactSection"
 import ProjectSection from "./ProjectSection"
-import ExperienceSection from "./ExperienceSection"
 import Navbar from "./Navbar"
-import SkillSection from "./SkillSection"
 import HorizontalLine from "./HorizontalLine"
 
 export default function Layout() {
@@ -13,12 +11,8 @@ export default function Layout() {
             <div className="md:pl-20">
                 <AboutSection />
                 <HorizontalLine />
-                {/* <SkillSection />
-                <HorizontalLine /> */}
                 <ProjectSection />
                 <HorizontalLine />
-                {/* <ExperienceSection />
-                <HorizontalLine /> */}
                 <ContactSection />
                 <HorizontalLine />
             </div>
