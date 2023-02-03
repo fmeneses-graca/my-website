8a965b89b40188e2d33884c60d138fcd0a61fac2
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2023-02-02 22:45:22 -0500

    Update contact section

--- components/ContactSection.js
+++ components/ContactSection.js
@@ -97,11 +97,10 @@ export default function ContactSection() {
 
     return (
         <div>
-
             <div id="contact" className="flex-container mb-10 md:mx-auto ">
-                <form onSubmit={handleSubmit} className="large-card flex flex-col px-8 py-8 rounded-lg bg-secondary bg-opacity-80 border border-slate-500">
+                <form onSubmit={handleSubmit} className="flex flex-col px-8 py-8 md:w-3/5 rounded-lg bg-secondary bg-opacity-80 border border-slate-500">
 
-                    <h1 className="text-2xl font-bold">Send me a message</h1>
+                    <h1 className="text-2xl font-bold">Let's work together!</h1>
 
                     <label htmlFor="fullname" className="mt-8">Full name</label>
                     <input type="text" value={fullname} onChange={(e) => { setFullname(e.target.value) }} name="fullname"
