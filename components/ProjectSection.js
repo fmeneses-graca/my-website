f64696e27b087693b1519f64b4f1af93233e8cfd
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2023-02-02 21:36:21 -0500

    Update project section with mercedes project

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -1,4 +1,6 @@
-
+import { IoOpen } from "react-icons/io5"
+import Card from "./card"
+import Link from 'next/link'
 
 export default function ProjectSection() {
     return (
@@ -6,9 +8,32 @@ export default function ProjectSection() {
             <div className="flex-col mx-auto">
                 <h1>Projects</h1>
 
-                <p className="text-center">Work in progress...</p>
+                <div className="flex-container">
+                    <Card style="medium-card" title="Mercedes-Benz Trucks Data Analytics Solution" text={mercedesProjectText()} />
+                </div>
 
             </div>
         </div>
+    )
+}
+
+function mercedesProjectText() {
+    return (
+        <>
+            <p className="pt-4">
+                In this MBA project, I created a Business Intelligence solution for Mercedes-Benz Trucks to drive data monetization.
+                The solution consists of a website that provides road contractors and public transportation agencies with insights about
+                road infrastructure to optimize construction investments.
+            </p>
+
+            <div className="flex">
+                <Link href="/mercedes-benz-project" passHref>
+                    <button className="icon group">
+                        <IoOpen size="28" />
+                    </button>
+                </Link>
+            </div>
+
+        </>
     )
 }
