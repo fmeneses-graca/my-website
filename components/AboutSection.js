3bc09bc6b7150312dd8a0f4f5d1bffbb9ce62c74
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2023-02-21 13:02:19 -0500

    About me

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -14,7 +14,7 @@ export default function AboutSection() {
                 <Image src={pptFramework} alt="People Process Technology Framework" width='400' className="mx-auto my-3" />
 
                 <p className="md:w-1/2 md:mx-auto">
-                    I&apos;m a Business and Data Analyst with a passion for working at the intersection of people, processes, and technology.
+                    I&apos;m a Business Intelligence and Data Analyst with a passion for working at the intersection of people, processes, and technology.
                     I help organizations reach their goals by making these three elements work together toward shared objectives using visual and analytical tools.
                     I have more than five years of experience analyzing data and designing solutions that make a lasting impact on stakeholders.
                 </p>
