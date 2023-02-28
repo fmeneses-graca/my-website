fd9575ffd41a0e3f82f6f1bf8de1e1c1ea12465b
Felipe Meneses Graca <97720492+fmeneses-graca@users.noreply.github.com>
2023-02-27 20:03:33 -0500

    Change about section

--- components/AboutSection.js
+++ components/AboutSection.js
@@ -14,9 +14,9 @@ export default function AboutSection() {
                 <Image src={pptFramework} alt="People Process Technology Framework" width='400' className="mx-auto my-3" />
 
                 <p className="md:w-1/2 md:mx-auto">
-                    I&apos;m a Business Intelligence and Data Analyst with a passion for working at the intersection of people, processes, and technology.
-                    I help organizations reach their goals by making these three elements work together toward shared objectives using visual and analytical tools.
-                    I have more than five years of experience analyzing data and designing solutions that make a lasting impact on stakeholders.
+                    I'm passionate about working at the intersection of people, processes, and technology. 
+                    I help organizations reach their goals by making these three elements work together toward shared objectives using visual and analytical tools. 
+                    I have more than five years of experience analyzing data and designing business solutions that make a lasting impact on stakeholders.
                 </p>
 
                 <br />
